4d382a98146f4492217004e8ec3a11f5552fd58f
tanmaygupta2009 <91407204+tanmaygupta2009@users.noreply.github.com>
2021-09-27 16:18:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var ball ;
+function setup() {
+
+  createCanvas(400,400);
+  ball = createSprite (200,200,20,20) ;
+  //ball.shapeColor = blue ;
+}
+
+function draw() 
+{
+  background("red");
+
+background(rgb(World. mouseX, World. mouseY, 0));
+    
+
+  drawSprites () ;
+
+}
+
+
+
+
